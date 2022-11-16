efaa66c5e6b51363b0bb4f1e3969b43fef61d586
anuridhir <108952808+anuridhir@users.noreply.github.com>
2022-11-16 21:49:53 +0530

    Update country.sql

--- country.sql
+++ country.sql
@@ -1,5 +1,5 @@
 
-create table countries(Country_ID int primary key,
+create table IF NOT EXISTS countries(Country_ID int primary key,
 Country_Name varchar(25),
 Region_ID char(25)
 
@@ -9,6 +9,7 @@ Region_ID char(25)
 insert into countries values(1,"Italy","ITA");
 insert into countries values(2,"India","IND");
 insert into countries values(3,"China","CHN");
+
 
 
 select * from countries;
